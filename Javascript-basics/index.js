8b24e35dd60773298204acae692980d25be01eae
Victor Nunes <victorngomes@gmail.com>
2022-10-26 21:13:16 -0300

    JS updated, included examples of Alert, Prompt, Confirm, GetElement methods, QuerySelector methods and InnerHTML

--- Javascript-basics/index.js
+++ Javascript-basics/index.js
@@ -1,12 +1,12 @@
+// ==================================
 // DOM EXAMPLE
+// ==================================
 
-const text = document.querySelector('p'); // Here we are collecting 'p' information of HTML page and inserting into 'text' constant
+//const text = document.querySelector('p');
+// Here we are collecting 'p' information of HTML page and inserting into 'text' constant
 
-text.innerHTML = 'Hello world with JS;' // And now we are changing the content from 'p', overwriting HTML definition
-text.style.color = 'red'; // Now changing the color using JS instead of CSS
+//text.innerHTML = 'Hello world with JS;'
+// And now we are changing the content from 'p', overwriting HTML definition
 
-// CONSOLE EXAMPLE
-
-//this part is all on the HTML document
-
-// ALERT, PROMPT & CONFIRM EXAMPLE
+//text.style.color = 'red';
+// Now changing the color using JS instead of CSS
