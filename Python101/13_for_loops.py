0b88bb8c50d5c70228b3de6798fb647caebbade9
Victor Nunes <victorngomes@gmail.com>
2023-05-23 16:52:43 -0300

    course update, more for examples added

--- Python101/13_for_loops.py
+++ Python101/13_for_loops.py
@@ -5,7 +5,7 @@
 print('')
 print('For loops')
 print('')
-# In for loops we already now how many interactions will be performed in advance
+# In for loops we already know how many interactions will be performed in advance
 for letter in 'Norwegian blue':  # 'letter' is the variable
     # the variable letter is used in 'for' loop to pick each one of characters in the string to write one by one
     print(letter)  # now the letters are getting printed one by one
@@ -43,11 +43,18 @@ for name in names:
     # the variable number is used in 'for' loop to pick each one of the names in the list names above
     print(name)
 
+produtos = ['coca', 'pepsi', 'guarana', 'sprite', 'fanta']
+producao = [15000, 12000, 13000, 5000, 250]
+
+print('Resultado da linha de produção de ontem: \n')
+for item in range(len(produtos)):  # gets the total elements using len function to print the information for each one
+    print(f'Produto: {produtos[item]} || Produção: {producao[item]} unidades')
+
 print('')
 print("For Loop done!")
 print('')
 
-# Break and Continue statements
+# Break and Continue statements (For Each)
 friends = ['John', 'Terry', 'Eric', 'Michael', 'George']
 for friend in friends:
     if friend == 'Eric':
@@ -72,7 +79,6 @@ print("For Loop done!")
 print('')
 
 # Nested loops - a loop inside another loop
-
 friends = ['John', 'Terry', 'Eric']
 for friend in friends:
     for number in [1, 2, 3]:
@@ -81,6 +87,13 @@ for friend in friends:
 print('')
 print("For Loop done!")
 print('')
+
+# Enumerate - runs a list and at the same time stores in a variable the index of each item
+# you should use only when is necessary to get the list index (because it is slower)
+
+funcionarios = ['José', 'Maria', 'João', 'Pedro', 'Joana', 'Francisca', 'Fernanda', 'Ana', 'Luiz']
+for i, funcionario in enumerate(funcionarios):
+    print(f'Index {i} é o funcionário {funcionario}')
 
 
 # ----------------------------
@@ -106,3 +119,62 @@ print('')
 
 for invite in invitation_list:
     print(f'{invite}! You are invited to the party on Saturday!')
+
+# =====================================
+
+estoque = [1200, 300, 800, 1500, 1900, 2750, 400, 20, 23, 70, 90, 80, 1100, 999, 900, 880, 870, 50, 1111, 120, 300, 450,
+           800]
+produtos = ['coca', 'pepsi', 'guarana', 'skol', 'brahma', 'agua', 'del valle', 'dolly', 'red bull', 'cachaça',
+            'vinho tinto', 'vodka', 'vinho branco', 'tequila', 'champagne', 'gin', 'guaracamp', 'matte',
+            'leite de castanha', 'leite', 'jurupinga', 'sprite', 'fanta']
+nivel_minimo = 50
+
+for i, qtde in enumerate(estoque):
+    if qtde < nivel_minimo:
+        print(f'O produto {produtos[i]} está abaixo do nível do estoque mínimo, temos apenas {estoque[i]} unidades')
+
+# =========================================
+"""
+# Exercícios
+
+## 1. Mudança de Carga Tributária
+
+- Reformas e mudanças de cargas tributárias são bem comuns no Brasil.
+
+Digamos que você trabalhe em uma empresa de ecommerce
+
+No Brasil, o imposto sobre livros é zerado. De um ano para o outro, o governo criou um novo imposto que incide em 10% sobre o valor dos livros e agora você precisa alterar o registro dos preços dos livros da empresa para garantir que esse imposto vai ser repassado para o preço final do produto.
+
+Crie um código que recalcule o valor do livro da sua lista de produtos e ajuste na tabela.
+
+Além disso, calcule qual vai ser o impacto financeiro da criação desse imposto para a empresa (ou seja, quanto que o imposto vai aumentar de custo para a empresa)
+
+Obs: para facilitar, colocamos apenas 1 livro na lista, mas em breve vamos aprender um for que vai adaptar esse cenário para qualquer quantidade de livros na sua lista.
+
+Obs2: Seu código deve funcionar mesmo que não haja livros na lista de produtos da empresa
+"""
+
+produtos = ['computador', 'livro', 'tablet', 'celular', 'tv', 'ar condicionado', 'alexa', 'máquina de café', 'kindle']
+
+# cada item da lista dos produtos corresponde a quantidade de vendas no mês e preço, nessa ordem
+produtos_ecommerce = [
+    [10000, 2500],
+    [50000, 40],
+    [7000, 1200],
+    [20000, 1500],
+    [5800, 1300],
+    [7200, 2500],
+    [200, 800],
+    [3300, 700],
+    [1900, 400]
+]
+
+if 'livro' in produtos:
+    i = produtos.index('livro')
+    antigo_total = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
+    produtos_ecommerce[i][1] = int(produtos_ecommerce[i][1] * 1.1)
+    novo_total = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
+    imposto = novo_total - antigo_total
+    print(f'O valor do livro subiu para R$ {produtos_ecommerce[i][1]} e o imposto é de R$ {imposto}')
+else:
+    print('Não há livros nos produtos')
