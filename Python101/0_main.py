8159b1e3cdc7354d65c4ba6db5c6db688d3e5efb
Victor Nunes <victorngomes@gmail.com>
2023-05-24 23:17:26 -0300

    for loops exercises file added one more example

---
+++ Python101/0_main.py
@@ -0,0 +1,3 @@
+funcionarios = ['José', 'Maria', 'João', 'Pedro', 'Joana', 'Francisca', 'Fernanda', 'Ana', 'Luiz']
+for i, funcionario in enumerate(funcionarios):
+    print(f'Index {i} é o funcionário {funcionario}')
--- Python101/13_for_loops_exercises.py
+++ Python101/13_for_loops_exercises.py
@@ -76,10 +76,15 @@ Lembrando, para calcular o % de crescimento de um produto de um ano para o outro
 Dica: lembre do enumerate, ele pode facilitar seu for
 """
 
-produtos = ['iphone', 'galaxy', 'ipad', 'tv', 'máquina de café', 'kindle', 'geladeira', 'adega', 'notebook dell',
-            'notebook hp', 'notebook asus', 'microsoft surface', 'webcam', 'caixa de som', 'microfone', 'câmera canon']
-vendas2019 = [558147, 712350, 573823, 405252, 718654, 531580, 973139, 892292, 422760, 154753, 887061, 438508, 237467,
-              489705, 328311, 591120]
-vendas2020 = [951642, 244295, 26964, 787604, 867660, 78830, 710331, 646016, 694913, 539704, 324831, 667179, 295633,
-              725316, 644622, 994303]
-
+# produtos = ['iphone', 'galaxy', 'ipad', 'tv', 'máquina de café', 'kindle', 'geladeira', 'adega', 'notebook dell',
+#             'notebook hp', 'notebook asus', 'microsoft surface', 'webcam', 'caixa de som', 'microfone', 'câmera canon']
+# vendas2019 = [558147, 712350, 573823, 405252, 718654, 531580, 973139, 892292, 422760, 154753, 887061, 438508, 237467,
+#               489705, 328311, 591120]
+# vendas2020 = [951642, 244295, 26964, 787604, 867660, 78830, 710331, 646016, 694913, 539704, 324831, 667179, 295633,
+#               725316, 644622, 994303]
+#
+# for i, produto in enumerate(produtos):
+#     if vendas2020[i] > vendas2019[i]:
+#         print(f'Produto: {produtos[i]}, Vendas 2019: {vendas2019[i]}, Vendas 2020: {vendas2020[i]}')
+#         print(f'Percentual de crescimento: {vendas2020[i] / vendas2019[i] - 1} %')
+#         print('\n')
