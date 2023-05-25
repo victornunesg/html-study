33ee6ed1cff77c5b532db381fc498e75a84f291f
Victor Nunes <victorngomes@gmail.com>
2023-05-25 17:56:21 -0300

    New for examples and concepts added. finished For Module.

--- Python101/13_for_loops.py
+++ Python101/13_for_loops.py
@@ -5,6 +5,7 @@
 print('')
 print('For loops')
 print('')
+
 # In for loops we already know how many interactions will be performed in advance
 for letter in 'Norwegian blue':  # 'letter' is the variable
     # the variable letter is used in 'for' loop to pick each one of characters in the string to write one by one
@@ -14,6 +15,8 @@ print('')
 print("For Loop done!")
 print('')
 
+###################################
+
 for number in range(8):
     # the variable number is used in 'for' loop to pick each one of the numbers from 0 to 7 to write one by one
     print(number)
@@ -21,6 +24,8 @@ for number in range(8):
 print('')
 print("For Loop done!")
 print('')
+
+###################################
 
 for step in range(1, 15, 3):
     # the variable number is used in 'for' loop to pick each one of the numbers from 1 to 15 in steps of 3
@@ -30,6 +35,8 @@ print('')
 print("For Loop done!")
 print('')
 
+###################################
+
 for name in ['John', 'Terry', 'Eric', 'Michael', 'George']:
     # the variable number is used in 'for' loop to pick each one of the names in the list above
     print(name)
@@ -38,10 +45,14 @@ print('')
 print("For Loop done!")
 print('')
 
+###################################
+
 names = ['John', 'Terry', 'Eric', 'Michael', 'George']
 for name in names:
     # the variable number is used in 'for' loop to pick each one of the names in the list names above
     print(name)
+
+###################################
 
 produtos = ['coca', 'pepsi', 'guarana', 'sprite', 'fanta']
 producao = [15000, 12000, 13000, 5000, 250]
@@ -53,6 +64,8 @@ for item in range(len(produtos)):  # gets the total elements using len function 
 print('')
 print("For Loop done!")
 print('')
+
+###################################
 
 # Break and Continue statements (For Each)
 friends = ['John', 'Terry', 'Eric', 'Michael', 'George']
@@ -66,6 +79,8 @@ print('')
 print("For Loop done!")
 print('')
 
+###################################
+
 friends = ['John', 'Terry', 'Eric', 'Michael', 'George']
 for friend in friends:
     if friend == 'Eric':
@@ -78,103 +93,50 @@ print('')
 print("For Loop done!")
 print('')
 
-# Nested loops - a loop inside another loop
+###################################
+
+# NESTED LOOPS - a loop inside another loop
+
 friends = ['John', 'Terry', 'Eric']
 for friend in friends:
-    for number in [1, 2, 3]:
+    print(friend) # this code will be executed 3 times since friends list has 3 elements only
+    for number in [1, 2, 3]:  # the variables needs to be different in each for loop
+        # this code will be executed 3 x 3 times, because there are 3 elements in each list of for loop
         print(friend, number)
 
 print('')
 print("For Loop done!")
 print('')
 
-# Enumerate - runs a list and at the same time stores in a variable the index of each item
+###################################
+
+# ENUMERATE
+
+# runs a list and at the same time stores in a variable the index of each item
 # you should use only when is necessary to get the list index (because it is slower)
 
 funcionarios = ['José', 'Maria', 'João', 'Pedro', 'Joana', 'Francisca', 'Fernanda', 'Ana', 'Luiz']
 for i, funcionario in enumerate(funcionarios):
     print(f'Index {i} é o funcionário {funcionario}')
 
+###################################
 
-# ----------------------------
-# EXERCISE
-# ----------------------------
+# BREAK AND CONTINUE
 
-print('')
-print('EXERCISE')
-print('')
-names = ['john ClEEse', 'Eric IDLE', 'michael']
-names1 = ['graHam chapman', 'TERRY', 'terry jones']
-invitation_list = []
-invitation_list.extend(names)
-invitation_list.extend(names1)
-print(invitation_list)
-print('')
+vendas = [100, 150, 1500, 2000, 120]
+vendedores = ['João', 'Julia', 'Ana', 'José', 'Maria']
 
-invitation_list.append(input('Enter the 1st extra name: '))
-invitation_list.append(input('Enter the 2nd extra name: '))
-invitation_list = (','.join(invitation_list)).title().split(',')
-print(invitation_list, type(invitation_list))
-print('')
+# Break: interrupts and finishes for loop, it goes to the next line outside for loop
+meta = 110
+for venda in vendas:
+    if venda < meta:
+        print('A loja não ganha bônus')
+        break
+    print(venda)  # não será executado
 
-for invite in invitation_list:
-    print(f'{invite}! You are invited to the party on Saturday!')
-
-# =====================================
-
-estoque = [1200, 300, 800, 1500, 1900, 2750, 400, 20, 23, 70, 90, 80, 1100, 999, 900, 880, 870, 50, 1111, 120, 300, 450,
-           800]
-produtos = ['coca', 'pepsi', 'guarana', 'skol', 'brahma', 'agua', 'del valle', 'dolly', 'red bull', 'cachaça',
-            'vinho tinto', 'vodka', 'vinho branco', 'tequila', 'champagne', 'gin', 'guaracamp', 'matte',
-            'leite de castanha', 'leite', 'jurupinga', 'sprite', 'fanta']
-nivel_minimo = 50
-
-for i, qtde in enumerate(estoque):
-    if qtde < nivel_minimo:
-        print(f'O produto {produtos[i]} está abaixo do nível do estoque mínimo, temos apenas {estoque[i]} unidades')
-
-# =========================================
-"""
-# Exercícios
-
-## 1. Mudança de Carga Tributária
-
-- Reformas e mudanças de cargas tributárias são bem comuns no Brasil.
-
-Digamos que você trabalhe em uma empresa de ecommerce
-
-No Brasil, o imposto sobre livros é zerado. De um ano para o outro, o governo criou um novo imposto que incide em 10% sobre o valor dos livros e agora você precisa alterar o registro dos preços dos livros da empresa para garantir que esse imposto vai ser repassado para o preço final do produto.
-
-Crie um código que recalcule o valor do livro da sua lista de produtos e ajuste na tabela.
-
-Além disso, calcule qual vai ser o impacto financeiro da criação desse imposto para a empresa (ou seja, quanto que o imposto vai aumentar de custo para a empresa)
-
-Obs: para facilitar, colocamos apenas 1 livro na lista, mas em breve vamos aprender um for que vai adaptar esse cenário para qualquer quantidade de livros na sua lista.
-
-Obs2: Seu código deve funcionar mesmo que não haja livros na lista de produtos da empresa
-"""
-
-produtos = ['computador', 'livro', 'tablet', 'celular', 'tv', 'ar condicionado', 'alexa', 'máquina de café', 'kindle']
-
-# cada item da lista dos produtos corresponde a quantidade de vendas no mês e preço, nessa ordem
-produtos_ecommerce = [
-    [10000, 2500],
-    [50000, 40],
-    [7000, 1200],
-    [20000, 1500],
-    [5800, 1300],
-    [7200, 2500],
-    [200, 800],
-    [3300, 700],
-    [1900, 400]
-]
-
-if 'livro' in produtos:
-    i = produtos.index('livro')
-    antigo_total = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
-    produtos_ecommerce[i][1] = int(produtos_ecommerce[i][1] * 1.1)
-    novo_total = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
-    imposto = novo_total - antigo_total
-    print(f'O valor do livro subiu para R$ {produtos_ecommerce[i][1]} e o imposto é de R$ {imposto}')
-else:
-    print('Não há livros nos produtos')
+# Continue: interrupts and goes to the next item inside for loop
+meta = 130
+for venda in vendas:
+    if venda < meta:
+        continue
+    print(venda)  # imprime somente os casos onde a venda > meta
--- Python101/13_for_loops_exercises.py
+++ Python101/13_for_loops_exercises.py
@@ -77,7 +77,8 @@ Dica: lembre do enumerate, ele pode facilitar seu for
 """
 
 # produtos = ['iphone', 'galaxy', 'ipad', 'tv', 'máquina de café', 'kindle', 'geladeira', 'adega', 'notebook dell',
-#             'notebook hp', 'notebook asus', 'microsoft surface', 'webcam', 'caixa de som', 'microfone', 'câmera canon']
+#             'notebook hp', 'notebook asus', 'microsoft surface', 'webcam', 'caixa de som',
+#             'microfone', 'câmera canon']
 # vendas2019 = [558147, 712350, 573823, 405252, 718654, 531580, 973139, 892292, 422760, 154753, 887061, 438508, 237467,
 #               489705, 328311, 591120]
 # vendas2020 = [951642, 244295, 26964, 787604, 867660, 78830, 710331, 646016, 694913, 539704, 324831, 667179, 295633,
@@ -88,3 +89,130 @@ Dica: lembre do enumerate, ele pode facilitar seu for
 #         print(f'Produto: {produtos[i]}, Vendas 2019: {vendas2019[i]}, Vendas 2020: {vendas2020[i]}')
 #         print(f'Percentual de crescimento: {vendas2020[i] / vendas2019[i] - 1} %')
 #         print('\n')
+
+
+""" 
+4. LISTA DE CONVIDADOS
+
+Preenche uma lista de convidados consolidando duas listas distintas;
+Adiciona dois convidados extra na lista consolidada;
+Arruma os nomes em letra maiúscula e printa no final os convidados separadamente.
+"""
+
+# names = ['john ClEEse', 'Eric IDLE', 'michael']
+# names1 = ['graHam chapman', 'TERRY', 'terry jones']
+# invitation_list = []
+# invitation_list.extend(names)
+# invitation_list.extend(names1)
+# print(invitation_list)
+# print('')
+#
+# invitation_list.append(input('Enter the 1st extra name: '))
+# invitation_list.append(input('Enter the 2nd extra name: '))
+# invitation_list = (','.join(invitation_list)).title().split(',')
+# print(invitation_list, type(invitation_list))
+# print('')
+#
+# for invite in invitation_list:
+#     print(f'{invite}! You are invited to the party on Saturday!')
+
+
+"""
+5. VERIFICAÇÃO DE ESTOQUE
+
+Verifica quais são os produtos que estão abaixo do estoque mínimo, baseado na comparação entre duas listas que refletem
+a lista de produtos e a quantidade atual em estoque.
+"""
+
+# estoque = [1200, 300, 800, 1500, 1900, 2750, 400, 20, 23, 70, 90, 80, 1100, 999, 900, 880, 870, 50, 1111, 120,
+#            300, 450, 800]
+# produtos = ['coca', 'pepsi', 'guarana', 'skol', 'brahma', 'agua', 'del valle', 'dolly', 'red bull', 'cachaça',
+#             'vinho tinto', 'vodka', 'vinho branco', 'tequila', 'champagne', 'gin', 'guaracamp', 'matte',
+#             'leite de castanha', 'leite', 'jurupinga', 'sprite', 'fanta']
+# nivel_minimo = 50
+#
+# for i, qtde in enumerate(estoque):
+#     if qtde < nivel_minimo:
+#         print(f'O produto {produtos[i]} está abaixo do nível do estoque mínimo, temos apenas {estoque[i]} unidades')
+
+"""
+6. VERIFICAÇÃO DE ESTOQUE (FOR DENTRO DE FOR)
+
+Vamos pegar um exemplo de nível mínimo de estoque. Em uma fábrica você tem vários produtos e não pode deixar que os 
+produtos fiquem em falta. Para isso, foi definido uma quantidade mínima de estoque que os produtos precisam ter:
+
+-Identifique quais fábricas tem algum produto abaixo do nível de estoque
+-Agora ao invés de analisar o estoque de apenas 1 fábrica, vamos analisar o estoque de várias fábricas
+
+"""
+#
+# estoque = [
+#     [294, 125, 269, 208, 783, 852, 259, 371, 47, 102, 386, 87, 685, 686, 697, 941, 163, 631, 7, 714, 218, 670, 453],
+#     [648, 816, 310, 555, 992, 643, 226, 319, 501, 23, 239, 42, 372, 441, 126, 645, 927, 911, 761, 445, 974, 2, 549],
+#     [832, 683, 784, 449, 977, 705, 198, 937, 729, 327, 339, 10, 975, 310, 95, 689, 137, 795, 211, 538, 933, 751, 522],
+#     [837, 168, 570, 397, 53, 297, 966, 714, 72, 737, 259, 629, 625, 469, 922, 305, 782, 243, 841, 848, 372, 621, 362],
+#     [429, 242, 53, 985, 406, 186, 198, 50, 501, 870, 781, 632, 781, 105, 644, 509, 401, 88, 961, 765, 422, 340, 654],
+# ]
+# fabricas = ['Lira Manufacturing', 'Fábrica Hashtag', 'Python Manufaturas', 'Produções e Cia', 'Manufatura e Cia']
+# nivel_minimo = 50
+#
+# fabricas_alerta = [] # criando lista auxiliar para armazenar as fábricas com estoque baixo
+# for i, lista in enumerate(estoque):
+#     for qtde in lista:
+#         if qtde < nivel_minimo:
+#             if fabricas[i] in fabricas_alerta:
+#                 pass
+#             else:
+#                 fabricas_alerta.append(fabricas[i])
+# print(fabricas_alerta)
+
+"""
+7. CALCULANDO O % DE UMA LISTA
+
+Faremos algo parecido com "filtrar" uma lista. Digamos que a gente tenha uma lista de vendedores e ao invés de saber 
+todos os vendedores que bateram a meta, eu quero conseguir calcular o % de vendedores que bateram a meta. 
+Ou seja, se temos 10 vendedores e 3 bateram a meta, temos 30% dos vendedores que bateram a meta.
+
+Vamos resolver de 2 formas:
+-Criando uma lista auxiliar apenas com os vendedores que bateram a meta
+-Fazendo o cálculo diretamente na lista que já temos
+-Para treinar uma estrutura parecida, crie um código para responder: quem foi o vendedor que mais vendeu?
+
+"""
+# meta = 10000
+# vendas = [
+#     ['João', 15000],
+#     ['Julia', 27000],
+#     ['Marcus', 9900],
+#     ['Maria', 3750],
+#     ['Ana', 10300],
+#     ['Alon', 7870],
+# ]
+#
+# # Criando uma lista auxiliar apenas com os vendedores que bateram a meta
+# meta_batida = []
+# for venda in vendas:
+#     if venda[1] >= meta:
+#         meta_batida.append(venda[0])
+# percentual = len(meta_batida) / len(vendas)
+# print(f'Lista de vendedores que bateram a meta: {meta_batida}')
+# print('Bateram a meta {:.0%} \n'.format(percentual))
+#
+# # Fazendo o cálculo diretamente na lista que já temos
+# contador = 0
+# for venda in vendas:
+#     if venda[1] >= meta:
+#         contador += 1
+# percentual2 = contador / len(vendas)
+# print(f'Qtde de vendedores que bateram a meta: {contador}')
+# print('Bateram a meta {:.0%} \n'.format(percentual2))
+#
+# # Para treinar uma estrutura parecida, crie um código para responder: quem foi o vendedor que mais vendeu?
+# vendedor = '0'
+# venda_max = 0
+# for venda in vendas:
+#     if venda[1] > venda_max:
+#         venda_max = venda[1]
+#         vendedor = venda[0]
+# print(f'O vendedor que mais vendeu: {vendedor}')
+# print(f'Quantidade de vendas: {venda_max}')
