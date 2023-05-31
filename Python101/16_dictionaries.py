21c1e71d2e079abb4f93fba0ca96dce5c2e2a6f2
Victor Nunes <victorngomes@gmail.com>
2023-05-30 23:04:09 -0300

    dictionary files updated with more examples

--- Python101/16_dictionaries.py
+++ Python101/16_dictionaries.py
@@ -1,22 +1,17 @@
 # ----------------------------
 # DICTIONARIES
 # ----------------------------
-print('')
-print('DICTIONARIES')
-print('')
 
+"""Dictionaries stores name/value pairs or key/value pairs
+You look for a word/key, so you get a definition/value
+It is like a translation table key -> value
+É possível obter uma informação através de uma chave ao invés de index (não recomendado pegar pelo index).
+Não deve ser usado para pegar itens em uma determinada ordem
+Podem ter valores heterogêneos (vários tipos de valores em um mesmo dicionário)
+As chaves são únicas, obrigatoriamente. Caso haja a mesma chave para dois valores diferentes, um será ignorado.
+"""
 
-# Dictionaries stores name/value pairs or key/value pairs
-# You look for a word/key, so you get a definition/value
-# It is like a translation table key -> value
-# Jan : January
-# Feb : February
-# É possível obter uma informação através de uma chave ao invés de index (não recomendado pegar pelo index).
-# Não deve ser usado para pegar itens em uma determinada ordem
-# Podem ter valores heterogêneos (vários tipos de valores em um mesmo dicionário)
-# As chaves são únicas, obrigatoriamente. Caso haja a mesma chave para dois valores diferentes, um será ignorado.
 # Let's define a movie dictionary:
-
 movie = {
     'title': 'Life of Brian',
     'year': 1979,
@@ -32,6 +27,22 @@ print(movie.get('budget'))  # the results will be an empty object, 'none'
 print(movie.get('budget', 'not found'))  # we also can set a default value for non-existent items
 print(movie)
 print('')
+"""
+O método GET deve ser usado sempre, para evitar erros caso não exista a chave a qual se busca
+Adicionalmente, em versões anteriores do Python a ordem do dicionário poderia sofrer alterações em tempo de compilação
+"""
+
+# Verificando se existe um item no dicionário - Opção 1
+if 'credit' in movie:  # Neste caso estamos procurando 'credit' nas chaves do dicionário
+    print(movie['credit'])
+else:
+    print('Credit does not exist in Movie')
+
+# Verificando se existe um item no dicionário - Opção 2
+if movie.get('credit') == None:  # Utilizando o método GET
+    print('Credit does not exist in Movie')
+else:
+    print(movie['credit'])
 
 # Updating an item and also defining new ones
 movie['title'] = 'The Holy Grail'  # updating an entry individually
@@ -104,5 +115,74 @@ print('')
 # calculating sum (only if values are numbers, otherwise an error will occur)
 print('The sum of the ages: ', sum(people.values()))
 print('')
+
+"""
+=================================
+MÉTODOS ESPECÍFICOS DE DICIONÁRIO
+=================================
+
+- clear() -> Deleta todos os elementos do Dicionário (semelhante ao que aprendemos em listas)
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+vendas_mes.clear()
+print(vendas_mes)
+
+- copy() -> Cria uma cópia do dicionário (semelhante ao que aprendemos em listas)
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+vendas2_mes = vendas_mes.copy()
+print(vendas2_mes)
+
+- fromkeys(chaves, valor_padrao) -> Cria um dicionário com as chaves e o mesmo valor padrão para todas as chaves
+chaves = ('jan', 'fev', 'mar')
+vendas = 100
+vendas_mes = dict.fromkeys(chaves, vendas)
+print(vendas_mes)
+
+- get(chave) -> 	Retorna o valor especificado pela chave (Semelhante a fazer dictionario[chave])
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+print(vendas_mes.get('mar'))
+
+- items() -> Retorna uma lista em que cada item é uma tupla com (chave, valor)
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+print(list(vendas_mes.items()))
+
+- keys() -> Retorna uma lista com todas as chaves do dicionário
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+print(list(vendas_mes.keys()))
+
+- pop(chave) -> Retira o item do dicionário e retorna o valor dele para ser usado
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+vendas_fev = vendas_mes.pop('fev') #retira o fevereiro do dicionário ao mesmo tempo que armazena o valor dele na variável
+print(vendas_mes)
+print(vendas_fev)
+
+- popitem() -> Retira o último item adicionado ao dicionário
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+#retira o último item adicionado no dicionário ao mesmo tempo que armazena o item(chave, valor) dele na variável
+vendas_ult = vendas_mes.popitem() 
+print(vendas_mes)
+print(vendas_ult)
+
+- setdefault(chave, valor) -> Retorna o valor da chave passada, mas caso a chave não exista, cria no dicionário o item com a chave e valor passados.
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+vendas_fev = vendas_mes.setdefault('fev', 500)
+print(vendas_fev)
+#repare que como fevereiro existe na lista, ele procura pelo valor de fevereiro e ignora o 500 passado
+#agora quando não existe na lista:
+vendas_abr = vendas_mes.setdefault('abr', 600)
+#repare que agora além de vendas_abr retornar o 600 como valor, ele adicionou um item no dicionario
+print(vendas_abr)
+print(vendas_mes)
+
+- update(dicionario) -> Adiciona o dicionário passado como parâmetro ao dicionário original
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+vendas_2tri = {'abr': 400, 'mai': 600, 'jun': 500}
+vendas_mes.update(vendas_2tri)
+print(vendas_mes)
+
+- values() -> Retorna uma lista com todos os valores do dicionários
+vendas_mes = {'jan': 150, 'fev': 100, 'mar': 190}
+print(list(vendas_mes.values()))
+
+"""
 
 
--- Python101/16_dictionaries_exercises.py
+++ Python101/16_dictionaries_exercises.py
@@ -19,71 +19,92 @@ stores into one...pretend Big Biz bought all the local stores, and want constant
 as in all games there is a special way to do this that actually makes money and solves the problem...
 can you find 'them'? Do you know why? May require knowledge of actual python 'lore'
 """
+# # create stores
+# freelancers = {
+#     'name': 'freelancing Shop', 'brian': 70, 'black knight': 20,
+#     'biccus diccus': 100, 'grim reaper': 500, 'minstrel': -15}
+#
+# antiques = {'name': 'Antique Shop', 'french castle': 400,
+#             'wooden grail': 3, 'scythe': 150, 'catapult': 75, 'german joke': 5}
+#
+# pet_shop = {'name': 'Pet Shop', 'blue parrot': 10, 'white rabbit': 5, 'newt': 2}
+#
+# # creating an empty dict to represent the cart
+# cart = {}
+#
+# # creating a purse to represent a credit to be used in the shopping
+# purse = 1000
+#
+# # this string will be used to inform the name and value of an item that has been bought
+# bought_items = ''
+#
+# # creating a dict containing all the stores inventory to inform how it is before the selling
+# department_store = {}
+# for department in (freelancers, antiques, pet_shop):
+#     department_store.update(department)
+# department_store.pop('name')
+# print('Morning inventory of stores:')
+# print(sorted(department_store.items()))
+# print('-----------------')
+#
+# # shopping begins, now we run through the shops, one by one
+# for shop in (freelancers, antiques, pet_shop):
+#     print('')
+#     print(f'Welcome to {shop["name"]}! These are the available items: ')
+#     print(f'{shop.keys()}'.lower())  # printing all shop items, in lowercase
+#     print('')
+#     buy_item = input('Enter the item to be bought or "exit" to go to the next shop: ')
+#
+#     # if user type other thing will be moved to the next loop/shop
+#     if buy_item not in shop or buy_item == 'exit':
+#         continue  # moves to the next loop/interaction
+#
+#     # updating the items bought adding the name and the value
+#     bought_items = bought_items + f'{buy_item}: {shop[buy_item]} Gp '
+#
+#     # updating the dict cart, adding the bought item via pop statement
+#     # which means that the item will be popped from the store
+#     cart.update({buy_item: shop.pop(buy_item)})
+#
+#     # bought_items = ", ".join(list(cart.keys()))  # creating a new variable to represent all the items bought
+#     # in this case we are turning the dictionary into a list first, then joining the list to get a string for better
+#     # representation to the user
+#
+# # is important to mention that using pop will remove all the item from the shop, including its value
+# # by this way we don`t have a value reduction, it removes key and value from the original dict
+#
+# print(cart)
+# total_cost = sum(cart.values())  # calculating the total amount
+# balance = purse - total_cost  # calculating the remaining balance
+# print('')
+# print(f'Congratulations, you`ve just bought: {bought_items}')  # here we are just printing the string of items bought
+# print(f'Total cost: {total_cost} Gp / Balance: {balance} Gp')  # informing user the shopping details
+#
+# # creating a dict containing all the stores inventory to inform how it is after the selling
+# department_store_after = {**freelancers, **antiques, **pet_shop}  # pyth 3.5
+# department_store_after.pop('name')
+# print('-----------------')
+# print('Evening inventory of stores', sorted(department_store_after.items()))
 
+"""
+Qual foi o item mais vendido nas categorias 'livros' e 'lazer'?
+Quanto foi vendido de 'notebook asus' e de 'ipad'?
+"""
+mais_vendidos = {'tecnologia': 'iphone', 'refrigeracao': 'ar consul 12000 btu', 'livros': 'o alquimista',
+                 'eletrodoméstico': 'geladeira', 'lazer': 'prancha surf'}
 
-# create stores
-freelancers = {
-    'name': 'freelancing Shop', 'brian': 70, 'black knight': 20,
-    'biccus diccus': 100, 'grim reaper': 500, 'minstrel': -15}
+vendas_tecnologia = {'iphone': 15000, 'samsung galaxy': 12000, 'tv samsung': 10000, 'ps5': 14300, 'tablet': 1720,
+                     'ipad': 1000, 'tv philco': 2500, 'notebook hp': 1000, 'notebook dell': 17000, 'notebook asus': 2450}
 
-antiques = {'name': 'Antique Shop', 'french castle': 400,
-            'wooden grail': 3, 'scythe': 150, 'catapult': 75, 'german joke': 5}
+livro = mais_vendidos['livros']
+lazer = mais_vendidos['lazer']
 
-pet_shop = {'name': 'Pet Shop', 'blue parrot': 10, 'white rabbit': 5, 'newt': 2}
+# Usando o método GET
 
-# creating an empty dict to represent the cart
-cart = {}
+notebook = vendas_tecnologia.get('notebook asus')
+ipad = vendas_tecnologia.get('ipad')
 
-# creating a purse to represent a credit to be used in the shopping
-purse = 1000
-
-# this string will be used to inform the name and value of an item that has been bought
-bought_items = ''
-
-# creating a dict containing all the stores inventory to inform how it is before the selling
-department_store = {}
-for department in (freelancers, antiques, pet_shop):
-    department_store.update(department)
-department_store.pop('name')
-print('Morning inventory of stores:')
-print(sorted(department_store.items()))
-print('-----------------')
-
-# shopping begins, now we run through the shops, one by one
-for shop in (freelancers, antiques, pet_shop):
-    print('')
-    print(f'Welcome to {shop["name"]}! These are the available items: ')
-    print(f'{shop.keys()}'.lower())  # printing all shop items, in lowercase
-    print('')
-    buy_item = input('Enter the item to be bought or "exit" to go to the next shop: ')
-
-    # if user type other thing will be moved to the next loop/shop
-    if buy_item not in shop or buy_item == 'exit':
-        continue  # moves to the next loop/interaction
-
-    # updating the items bought adding the name and the value
-    bought_items = bought_items + f'{buy_item}: {shop[buy_item]} Gp '
-
-    # updating the dict cart, adding the bought item via pop statement
-    # which means that the item will be popped from the store
-    cart.update({buy_item: shop.pop(buy_item)})
-
-    # bought_items = ", ".join(list(cart.keys()))  # creating a new variable to represent all the items bought
-    # in this case we are turning the dictionary into a list first, then joining the list to get a string for better
-    # representation to the user
-
-# is important to mention that using pop will remove all the item from the shop, including its value
-# by this way we don`t have a value reduction, it removes key and value from the original dict
-
-print(cart)
-total_cost = sum(cart.values())  # calculating the total amount
-balance = purse - total_cost  # calculating the remaining balance
-print('')
-print(f'Congratulations, you`ve just bought: {bought_items}')  # here we are just printing the string of items bought
-print(f'Total cost: {total_cost} Gp / Balance: {balance} Gp')  # informing user the shopping details
-
-# creating a dict containing all the stores inventory to inform how it is after the selling
-department_store_after = {**freelancers, **antiques, **pet_shop}  # pyth 3.5
-department_store_after.pop('name')
-print('-----------------')
-print('Evening inventory of stores', sorted(department_store_after.items()))
+print(f'Livro mais vendido: {livro}')
+print(f'Lazer mais vendido: {lazer}')
+print(f'Vendas de Notebook Asus: {notebook} unidades')
+print(f'Vendas de iPad: {ipad} unidades')
