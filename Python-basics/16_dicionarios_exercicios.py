f68d88319be2048c7f4685c0e11d214922d255ae
Victor Nunes <victorngomes@gmail.com>
2023-11-22 14:30:35 -0300

    lists and dictionaries exercises updated

--- Python-basics/16_dicionarios_exercicios.py
+++ Python-basics/16_dicionarios_exercicios.py
@@ -286,3 +286,113 @@ etapas """
 # vendas_produtos = zip(produtos, vendas)
 # dicionario = dict(vendas_produtos)
 # print(dicionario)
+
+'''Exercício 1
+Crie um sistema de consulta de preços
+Seu sistema deve:
+- Pedir para o usuário o nome de um produto
+- Caso o produto exista na lista de produtos, o programa deve retornar o preço do produto como resposta
+       - Ex: O produto celular custa R$1500
+- Caso o produto não exista na lista de produtos, o programa deve printar uma mensagem para o usuário tentar novamente'''
+
+precos = {"celular": 1500, "camera": 1000, "fone de ouvido": 800, "monitor": 2000}
+
+# produto = input("Digite o nome do produto: ").lower()
+# while precos.get(produto) is None:
+#     print("Produto não encontrado, tente novamente!")
+#     produto = input("\nDigite o nome do produto: ").lower()
+# else:
+#     print(f'O produto {produto} custa R$ {precos.get(produto)}')
+
+'''Exercício 2
+Agora edite o programa anterior para fazer com que, caso não exista o produto, o programa pergunte se o usuário quer cadastrar o produto
+Se ele responder sim, o programa deve pedir o nome do produto e o preco do produto e cadastrar no dicionário de preços
+Em seguida do cadastro bem sucedido, o programa deve printar o dicionário de precos atualizado'''
+
+# produto = input("Digite o nome do produto: ").lower()
+# while precos.get(produto) is None:
+#     resposta = input("Produto não encontrado, deseja cadastrar um novo produto? (sim/não): ").lower()
+#     if resposta == 'sim':
+#         novo_produto = input("Digite o nome do novo produto a ser cadastrado: ").lower()
+#         preco = int(input("Digite o preço do novo produto a ser cadastrado: "))
+#         precos[novo_produto] = preco
+#         print(f'\nCadastro realizado com sucesso! {precos}')
+#         exit()
+#     else:
+#         produto = input("\nDigite o nome do produto: ").lower()
+# else:
+#     print(f'O produto {produto} custa R$ {precos.get(produto)}')
+
+'''Exercício 3
+Dada a lista de preços de produtos, uma loja resolveu fazer um reajuste nos preços dos produtos. 
+calcule o novo valor dos produtos com base nas seguintes regras:
+Preços até 1.000 vão ter um reajuste de 10% (ou seja, o novo preço será 110% do preço atual)
+Preços até maiores que 1.000 até 2.000 vão ter reajuste de 15%
+Preços acima de 2.000 vão ter reajuste de 20%'''
+precos = {"celular": 1500, "camera": 1000, "fone de ouvido": 800, "monitor": 3000}
+
+# for produto in precos:
+#     if precos[produto] < 1000:
+#         precos[produto] *= 1.1
+#     elif 1000 <= precos[produto] < 2000:
+#         precos[produto] *= 1.15
+#     else:
+#         precos[produto] *= 1.2
+#
+# print(precos)
+
+'''Exercício 4
+Edite o programa antigo para ter os 2 dicionários, o de preços originais e o de novos preços
+Em seguida calcule o valor total de reajuste em R$ que teve entre a lista de produtos original e a lista final'''
+
+# precos_reajuste = {}
+# precos_reajuste.update(precos)
+#
+# for produto in precos:
+#     if precos[produto] < 1000:
+#         precos_reajuste[produto] *= 1.1
+#     elif 1000 <= precos[produto] < 2000:
+#         precos_reajuste[produto]*= 1.15
+#     else:
+#         precos_reajuste[produto] *= 1.2
+#
+# print(f'Valores antes do reajuste: {precos} / Total: R$ {sum(precos.values())}')
+# print(f'Valores depois do reajuste: {precos_reajuste} / Total: R$ {sum(precos_reajuste.values())}')
+#
+# print(f'\nValor total de reajuste: R$ {sum(precos_reajuste.values()) - sum(precos.values())}')
+
+'''Exercício 5
+Uma empresa está analisando os resultados de vendas do 1º semestre de 2022 e 2023
+Qual foi o % de crescimento de cada mês de 2023 em relação a 2022?
+Depois de calcular isso, calcule o valor total de crescimento de 2023 em relação a 2022'''
+
+# vendas_22 = {"jan": 15000, "fev": 15500, "mar": 14000, "abr": 16600, "mai": 16300, "jun": 17000}
+# vendas_23 = {"jan": 17000, "fev": 15000, "mar": 17500, "abr": 16900, "mai": 16000, "jun": 18500}
+#
+# total_22 = sum(vendas_22.values())
+# total_23 = sum(vendas_23.values())
+# crescimento = total_23 - total_22
+#
+# for mes in vendas_23:
+#     print(f'Crescimento em {mes}: {(vendas_23[mes] / vendas_22[mes]) -1 :.2%}')
+#
+# print(f'\nValor total de crescimento: {crescimento}')
+
+'''Exercício 6 - Desafio
+No final da reunião de apresentação dos números, seu chefe perguntou:
+E se nos meses de 2023 que a gente vendeu menos do que 2022 a gente tivesse pelo menos empatado com 2022 (ou seja, se nos meses de 2023 em que as vendas foram menores do que o mesmo mês em 2022, o valor de vendas tivesse sido igual a 2022)
+Qual teria sido o nosso crescimento de 2023 frente a 2022?'''
+
+# vendas_22 = {"jan": 15000, "fev": 15500, "mar": 14000, "abr": 16600, "mai": 16300, "jun": 17000}
+# vendas_23 = {"jan": 17000, "fev": 15000, "mar": 17500, "abr": 16900, "mai": 16000, "jun": 18500}
+#
+# for mes in vendas_23:
+#     if vendas_23[mes] < vendas_22[mes]:
+#         vendas_23[mes] = vendas_22[mes]
+#
+# total_22 = sum(vendas_22.values())
+# total_23 = sum(vendas_23.values())
+# crescimento = total_23 - total_22
+#
+# print(f'\nValor total de crescimento: {crescimento}')
+
--- Python-basics/6_listas.py
+++ Python-basics/6_listas.py
@@ -279,43 +279,107 @@ Resultado:
 # Exercícios
 
 ## 1. Mudança de Carga Tributária
-
 - Reformas e mudanças de cargas tributárias são bem comuns no Brasil.
-
 Digamos que você trabalhe em uma empresa de ecommerce
-
 No Brasil, o imposto sobre livros é zerado. De um ano para o outro, o governo criou um novo imposto que incide em 10% sobre o valor dos livros e agora você precisa alterar o registro dos preços dos livros da empresa para garantir que esse imposto vai ser repassado para o preço final do produto.
-
 Crie um código que recalcule o valor do livro da sua lista de produtos e ajuste na tabela.
-
 Além disso, calcule qual vai ser o impacto financeiro da criação desse imposto para a empresa (ou seja, quanto que o imposto vai aumentar de custo para a empresa)
-
 Obs: para facilitar, colocamos apenas 1 livro na lista, mas em breve vamos aprender um for que vai adaptar esse cenário para qualquer quantidade de livros na sua lista.
-
 Obs2: Seu código deve funcionar mesmo que não haja livros na lista de produtos da empresa
 """
 
-produtos = ['computador', 'livro', 'tablet', 'celular', 'tv', 'ar condicionado', 'alexa', 'máquina de café', 'kindle']
+# produtos = ['computador', 'livro', 'tablet', 'celular', 'tv', 'ar condicionado', 'alexa', 'máquina de café', 'kindle']
+#
+# # cada item da lista dos produtos corresponde a quantidade de vendas no mês e preço, nessa ordem
+# produtos_ecommerce = [
+#     [10000, 2500],
+#     [50000, 40],
+#     [7000, 1200],
+#     [20000, 1500],
+#     [5800, 1300],
+#     [7200, 2500],
+#     [200, 800],
+#     [3300, 700],
+#     [1900, 400]
+# ]
+#
+# if 'livro' in produtos:
+#     i = produtos.index('livro')
+#     antigo_total = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
+#     produtos_ecommerce[i][1] = int(produtos_ecommerce[i][1] * 1.1)
+#     novo_total = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
+#     imposto = novo_total - antigo_total
+#     print(f'O valor do livro subiu para R$ {produtos_ecommerce[i][1]} e o imposto é de R$ {imposto}')
+# else:
+#     print('Não há livros nos produtos')
 
-# cada item da lista dos produtos corresponde a quantidade de vendas no mês e preço, nessa ordem
-produtos_ecommerce = [
-    [10000, 2500],
-    [50000, 40],
-    [7000, 1200],
-    [20000, 1500],
-    [5800, 1300],
-    [7200, 2500],
-    [200, 800],
-    [3300, 700],
-    [1900, 400]
+'''Exercício 1
+Crie um sistema de cadastro de produtos em uma lista de produtos
+Seu sistema deve:
+- Pegar o usuário qual produto vai ser cadastrado por meio de um input
+- Garantir que se o usuário escrever com letra maiúscula ou minúscula, o produto continua sendo o mesmo produto
+- Se o usuário inserir um produto que já existe na lista, o programa deve printar a mensagem "Produto já existente, tente novamente"
+- Se o usuário inserir um produto que não existe na lista, o programa deve inserir na lista, printar a mensagem Produto X cadastrado com sucesso e em seguida printar a lista completa'''
+
+# produtos = ["celular", "camera", "fone de ouvido", "monitor"]
+# produto = input("Entre com o nome do produto: ").lower()
+# while produto in produtos:
+#     print("Produto já existente, tente novamente")
+#     print("")
+#     produto = input("Entre com o nome do produto: ").lower()
+# produtos.append(produto)
+# print(f"\nProduto {produto} cadastrado com sucesso:")
+# print(produtos)
+
+'''Exercício 2
+Crie um sistema de consulta de preços
+Seu sistema deve:
+- Pedir para o usuário o nome de um produto
+- Caso o produto exista na lista de produtos, o programa deve retornar o preço do produto como resposta
+       - Ex: O produto celular custa R$1500
+- Caso o produto não exista na lista de produtos, o programa deve printar uma mensagem para o usuário tentar novamente'''
+
+# produtos = ["celular", "camera", "fone de ouvido", "monitor"]
+# precos = [1500, 1000, 800, 2000]
+#
+# produto = input("Entre com o nome do produto: ").lower()
+# if produto in produtos:
+#     print(f'O produto {produto} custa R$ {precos[produtos.index(produto)]}')
+# else:
+#     print(f"O produto {produto} não existe na lista de produtos. Tente novamente!")
+
+'''Exercício 3
+Crie um sistema de consulta de bônus dos funcionários
+Seu sistema deve:
+- Pegar o valor de vendas do funcinoário por meio de um input
+- Calcular o bônus do funcionário de acordo com a seguinte regra:
+      - Se o funcionário vendeu mais de 1000 unidades, ele ganha R$2 de bonus para cada unidade vendida
+      - Se o funcionário vendeu mais de 5000 unidades, ele ganha R$2 de bônus para cada unidade + um valor fixo de R$1000
+      - Se o funcionário vendeu menos de 1000 unidades, ele não ganha bônus
+- Printar no final o valor do bônus do funcionário'''
+
+# vendas = int(input("Valor de vendas do funcionário: "))
+# if vendas <= 1000:
+#     bonus = 0
+# elif 1000 < vendas <= 5000:
+#     bonus = vendas * 2
+# elif vendas > 5000:
+#     bonus = (vendas * 2) + 1000
+#
+# print(f"\nBônus do funcionário: R$ {bonus}")
+
+'''Exercício 4
+Crie um programa que consiga descobrir qual dos vendedores vendeu mais
+As vendas dos vendedores são listas com a quantidade vendida por cada vendedor'''
+
+vendas = [
+    [10, 20, 100, 80, 90, 100, 20, 30, 44, 55, 33, 34, 100, 90, 80, 39, 87, 45, 50, 50, 50, 50, 40, 30, 3, 93, 39, 49, 88],
+    [100, 1, 1, 4, 5, 90, 100, 20, 4, 5, 100, 100, 100, 100, 100, 93, 20, 15, 40, 90, 90, 90, 90, 90, 90, 33, 22, 44, 43, 34],
 ]
 
-if 'livro' in produtos:
-    i = produtos.index('livro')
-    antigo_total = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
-    produtos_ecommerce[i][1] = int(produtos_ecommerce[i][1] * 1.1)
-    novo_total = produtos_ecommerce[i][0] * produtos_ecommerce[i][1]
-    imposto = novo_total - antigo_total
-    print(f'O valor do livro subiu para R$ {produtos_ecommerce[i][1]} e o imposto é de R$ {imposto}')
-else:
-    print('Não há livros nos produtos')
+# total_vendas = []
+# print(len(vendas))
+# for i in range(len(vendas)):
+#     total_vendas.append(sum(vendas[i]))
+# print(total_vendas)
+# print(f'O vendedor vencedor: #{total_vendas.index(max(total_vendas))+1}')
